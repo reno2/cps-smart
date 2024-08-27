01237e2240a5e673ed7bba9f424153321bbc8379
a.zharsky <a.zharsky@gazpromcps.ru>
2024-08-27 17:12:17 +0300

    feat(request): add request class and check if has payload key

--- src/index.php
+++ src/index.php
@@ -1,2 +1,21 @@
 <?
-phpinfo();
+
+
+use Cps\Smart\Request;
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+$request = new Request();
+$request->setRouteParams($request->getBody());
+
+
+if ($request->isPost()) {
+    if ($payload = $request->getRouteParam('payload')) {
+        echo $payload;
+    }
+}
+
+
+
+
+
